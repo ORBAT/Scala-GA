63746e072c2baec801ea02d7318b2787f7a77129
Tom Eklof <tom.eklof@uplause.com>
2013-07-27 22:49:35 +0300

    SimpleStack now uses an ArrayStack for backing.

--- src/scagen2/vm/SimpleStack.scala
+++ src/scagen2/vm/SimpleStack.scala
@@ -1,99 +1,81 @@
 package scagen2.vm
 
 object SimpleStack {
-   type ItemType = Double
-   protected[SimpleStack] val defaultContent: ItemType = Double.NaN
- }
+  type ItemType = Double
+  protected[SimpleStack] val defaultContent: ItemType = Double.NaN
+}
 
 class SimpleStack {
 
-   import SimpleStack._
+  import SimpleStack._
 
-   private[this] var stack = Vector.empty[ItemType]
+  private[this] val stack = collection.mutable.ArrayStack[ItemType]()
 
-   def clear() {
-     stack = Vector.empty
-   }
+  def clear() {
+    stack.clear()
+  }
 
-   def push(i: ItemType) {
-     stack = i +: stack
-   }
+  def push(i: ItemType) {
+    stack.push(i)
+  }
 
-   def pop() = {
-     val length = stack.length
-     val ret =
-       if(length == 0)
-         defaultContent
-       else {
-         val out = stack.head
-         stack = stack.tail
-         out
-       }
-     ret
-   }
+  def pop() = {
+    val ret =
+      if(stack.length == 0)
+        defaultContent
+      else {
+        stack.pop()
+      }
+    ret
+  }
 
-   def dup() {
-     if(stack.length != 0) {
-       val item = peek
-       push(item)
-     }
-   }
+  def dup() {
+    if(stack.length != 0) {
+      stack.dup()
+    }
+  }
 
-   def drop() {
-     stack = stack.tail
-   }
+  def drop() {
+    if(stack.length != 0)
+      stack.pop() // note that this is discarded since drop returns Unit
+  }
 
-   /**
-    * swap ( a b -- b a )
-    */
-   def swap() {
-     val first = stack.head
-     val second = stack.tail.head
-     stack = second +: first +: stack.drop(2)
-   }
+  /**
+   * swap ( a b -- b a )
+   */
+  def swap() {
+    if(stack.length > 2) {
+      val first = stack.pop()
+      val second = stack.pop()
+      stack.push(first)
+      stack.push(second)
+    }
+  }
 
-   /**
-    * over ( a b -- a b a )
-    */
-   def over() {
-     if(stack.length > 0) {
-       push(stack.last)
-     }
-   }
+  def peek = {
+    val ret = if(stack.length == 0)
+      defaultContent
+    else {
+      stack.top
+    }
+    ret
+  }
 
-   def rot() {
-     if(stack.length > 1) {
-       val last = stack.last
-       stack = stack.dropRight(1)
-       stack = last +: stack
-     }
-   }
+  def length = stack.length
 
-   def peek = {
-     val length = stack.length
-     val ret = if(length == 0)
-       defaultContent
-     else {
-       stack.head
-     }
-     ret
-   }
+  /*
 
-   def length = stack.length
+ peek ( a -- a )
+ pop ( a -- )
+ push ( -- a )
 
-   /*
+  dup  ( a -- a a )
+drop ( a -- )
+swap ( a b -- b a )
+over ( a b -- a b a )
+rot  ( a b c -- b c a )
 
-  peek ( a -- a )
-  pop ( a -- )
-  push ( -- a )
-
-   dup  ( a -- a a )
- drop ( a -- )
- swap ( a b -- b a )
- over ( a b -- a b a )
- rot  ( a b c -- b c a )
-
-    */
+   */
 
 
- }
+}
