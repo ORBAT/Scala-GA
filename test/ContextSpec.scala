f5b0eafa7b322110bff584f88051086cd5067ac8
Tom Eklöf <tomeklof+github@gmail.com>
2013-10-23 01:39:39 +0300

    Test stub for Context

---
+++ test/ContextSpec.scala
@@ -0,0 +1,12 @@
+import org.scalatest.FlatSpec
+import scagen2.utils._
+
+class ContextSpec extends FlatSpec {
+
+  behavior of "A Gene with Context"
+
+  it should "Support JNZ" in {
+    fail
+  }
+
+}
